6492c4920c510bbfe1cd56283e9e2c11b0c95832
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-08-25 19:12:16 -0300

    Exercício 3

--- Fundamentos/Bloco 04/Introdução à JavaScript e Lógica de Programação/Dia 01/script.js
+++ Fundamentos/Bloco 04/Introdução à JavaScript e Lógica de Programação/Dia 01/script.js
@@ -9,11 +9,24 @@
 // console.log(a % b)
 
 // exercício 2
-const num1 = 7;
-const num2 = 5;
+// const num1 = 7;
+// const num2 = 5;
 
-if (num1 > num2) {
+// if (num1 > num2) {
+//     console.log(num1)
+// } else {
+//     console.log(num2)
+// }
+
+// exercício 3
+const num1 = 3
+const num2 = 6
+const num3 = 2
+
+if (num1 > num2 && num1 > num3) {
     console.log(num1)
-} else {
+} else if (num2 > num1 && num2 > num3) {
     console.log(num2)
+} else {
+    console.log(num3)
 }
