60646da54b8491ce0e8e478d1ca4c6240b9596f1
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-08-25 19:05:41 -0300

    Exercício 2

--- Fundamentos/Bloco 04/Introdução à JavaScript e Lógica de Programação/Dia 01/script.js
+++ Fundamentos/Bloco 04/Introdução à JavaScript e Lógica de Programação/Dia 01/script.js
@@ -1,9 +1,19 @@
 // exercício 1
-const a = 22;
-const b = 18;
+// const a = 22;
+// const b = 18;
 
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log(a % b)
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// exercício 2
+const num1 = 7;
+const num2 = 5;
+
+if (num1 > num2) {
+    console.log(num1)
+} else {
+    console.log(num2)
+}
