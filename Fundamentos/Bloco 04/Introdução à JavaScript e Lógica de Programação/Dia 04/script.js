4b7f6d8aa9ce6563076b4f23df58c3ca469b0d22
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-08-30 12:11:06 -0300

    Primeira Parte dos exercicios

---
+++ Fundamentos/Bloco 04/Introdução à JavaScript e Lógica de Programação/Dia 04/script.js
@@ -0,0 +1,65 @@
+// Parte 1
+
+// exercicio 1
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// console.log("Bem vinda " + info.personagem)
+
+// exercicio 2 
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: "sim",
+// };
+
+// console.log(info)
+
+// exercicio 3
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: "sim",
+// };
+
+// for (let index in info) {
+//   console.log(index)
+// }
+
+// exercicio 4
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: "Sim",
+// };
+
+// for (let index in info) {
+//   console.log(info[index])
+// }
+
+// exercicio 5 
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: "Sim",
+// };
+
+// let segundoObjeto = {
+//   personagem:'Tio Patinhas',
+//   origem:"Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota:'O último MacPatinhas',
+//   recorrente:'Sim',
+// }
+
+// for (let index in info,segundoObjeto) {
+//   console.log(info[index] + " e " + segundoObjeto[index])
+// }
+
+
