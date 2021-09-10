ff1b7d7596eba510babe38f9574094259cd1c7a4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-10 12:29:08 -0300

    Exercicio 1 5.3

--- Fundamentos/Bloco 5 JavaScript: DOM, Eventos e Web Storage/Dia 03 JavaScript: DOM, Eventos e Web Storage/script.js
+++ Fundamentos/Bloco 5 JavaScript: DOM, Eventos e Web Storage/Dia 03 JavaScript: DOM, Eventos e Web Storage/script.js
@@ -13,4 +13,33 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+// Exercício 1.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+function DaysOfCalendary () {
+  let ul = document.querySelector('#days');  // recuperar Ul
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    if (dezDaysList[index] === 24 || dezDaysList[index] === 31) {
+      let li = document.createElement('li');  // criar Li
+      li.className = 'day holiday'; // Adicionar Classe a Li
+      li.innerText = dezDaysList[index]; // Adiciona a posição index do array de Dias em Li
+      ul.appendChild(li); // Adiciona Li filha de Ul 
+    } else if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18) {
+      let li = document.createElement('li');  // criar Li
+      li.className = 'day friday'; // Adicionar Classe a Li
+      li.innerText = dezDaysList[index]; // Adiciona a posição index do array de Dias em Li
+      ul.appendChild(li); // Adiciona Li filha de Ul    
+
+    } else if (dezDaysList[index] === 25) {
+      let li = document.createElement('li');  // criar Li
+      li.className = 'day holiday friday'; // Adicionar Classe a Li
+      li.innerText = dezDaysList[index]; // Adiciona a posição index do array de Dias em Li
+      ul.appendChild(li); // Adiciona Li filha de Ul 
+    } else {
+      let li = document.createElement('li');  // criar Li
+      li.className = 'day'; // Adicionar Classe a Li
+      li.innerText = dezDaysList[index]; // Adiciona a posição index do array de Dias em Li
+      ul.appendChild(li); // Adiciona Li filha de Ul 
+    }
+  }
+}
+DaysOfCalendary ()
