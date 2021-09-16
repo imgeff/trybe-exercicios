5836274c14f6bcc81d07b8a7d064549e73e64e22
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-16 10:19:56 -0300

    Exercício 1

---
+++ Fundamentos/Bloco 06  HTML e CSS: Forms, Flexbox e Responsivo/Dia 01 - Forms/script.js
@@ -0,0 +1,10 @@
+// select -  estado 
+let select = document.querySelector('#estado-user');
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', '*Distrito Federal'];
+let siglas = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
+for (let index = 0; index <= 26; index += 1) {
+  let option = document.createElement('option');
+  option.value = siglas[index];
+  option.innerText = estados[index];
+  select.appendChild(option);
+}
