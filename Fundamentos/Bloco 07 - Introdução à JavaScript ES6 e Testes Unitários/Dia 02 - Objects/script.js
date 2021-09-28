47c7a1c18e971f5162494125b058fad56196aeaf
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-27 21:46:46 -0300

    Exercicio 2 - add key

--- Fundamentos/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 02 - Objects/script.js
+++ Fundamentos/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 02 - Objects/script.js
@@ -50,3 +50,24 @@ return `Olá ${nome}, o total do seu pedido de ${Object.keys(order.order.pizza)}
 }
 
 console.log(orderModifier(order));
+
+// Exercicio 2
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
