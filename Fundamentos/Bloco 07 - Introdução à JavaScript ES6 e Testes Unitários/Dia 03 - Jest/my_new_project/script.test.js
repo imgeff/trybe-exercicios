6a055e558603c9eb216b11bbf905e90041fe0936
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-28 19:25:18 -0300

    Exercicio 1 sum(0, 0)

--- Fundamentos/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 03 - Jest/my_new_project/script.test.js
+++ Fundamentos/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 03 - Jest/my_new_project/script.test.js
@@ -4,4 +4,8 @@ describe('Função sum', () => {
   it('Testa se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4, 5)).toBe(9)
   })
+
+  it('Testa se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toBe(0)
+  })
 })
