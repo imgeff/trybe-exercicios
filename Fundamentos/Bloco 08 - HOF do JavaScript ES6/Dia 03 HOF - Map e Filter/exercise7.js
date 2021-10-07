e40f3f42d8897d88f663a6d49d5ccc199bc712d1
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-07 12:25:57 -0300

    Exercício 7

--- Fundamentos/Bloco 08 - HOF do JavaScript ES6/Dia 03 HOF - Map e Filter/exercise7.js
+++ Fundamentos/Bloco 08 - HOF do JavaScript ES6/Dia 03 HOF - Map e Filter/exercise7.js
@@ -62,3 +62,13 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  return books.find((book) => (
+    book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3
+  )).name;
+}
+
+console.log (authorWith3DotsOnName())
