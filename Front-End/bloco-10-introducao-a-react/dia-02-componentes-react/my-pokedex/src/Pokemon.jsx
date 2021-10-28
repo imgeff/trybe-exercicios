6db424c0837f6fbaebe704d6ff7f79c7b43b09e8
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-28 20:40:04 -0300

    Cria e estrutura componente Pokemon

---
+++ Front-End/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Pokemon extends React.Component {
+  render() {
+    return(
+      <section>
+        <p>Nome do pokemon</p>
+        <p>tipo do pokemon</p>
+        <p>peso médio do pokemon</p>
+        <p>imagem do pokemon</p>
+      </section>
+    );
+  }
+}
+
+export default Pokemon;
