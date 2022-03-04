b3d5cd06cbf3a88a5efa457aaed7ca75c5f940b8
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-04 15:06:10 -0300

    feat(innerJoin): Query 2

--- Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-02-descomplicando-JOINs/exercicios-de-fixacao/inner-join/script.sql
+++ Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-02-descomplicando-JOINs/exercicios-de-fixacao/inner-join/script.sql
@@ -4,6 +4,10 @@ FROM sakila.actor act
 INNER JOIN sakila.film_actor fact
 ON act.actor_id = fact.actor_id;
 -- QUERY 2
+SELECT stf.first_name, stf.last_name, adr.address
+FROM sakila.staff stf
+INNER JOIN sakila.address adr
+ON stf.address_id = adr.address_id;
 -- QUERY 3
 -- QUERY 4
 -- QUERY 5
