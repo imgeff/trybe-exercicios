b7ac68c5f39dd92150cb42f2bacd72534a3d8781
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-04 21:11:10 -0300

    feat(exerciseOfDay): Query 2

--- Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-02-descomplicando-JOINs/script.sql
+++ Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-02-descomplicando-JOINs/script.sql
@@ -3,3 +3,9 @@ SELECT m.title, BO.domestic_sales, BO.international_sales
 FROM Pixar.Movies m
 INNER JOIN Pixar.BoxOffice BO
 ON m.id = BO.movie_id;
+-- QUERY 2
+SELECT m.title, BO.domestic_sales + BO.international_sales AS number_sales
+FROM Pixar.Movies m
+INNER JOIN Pixar.BoxOffice BO
+ON m.id = BO.movie_id
+WHERE BO.international_sales > BO.domestic_sales;
