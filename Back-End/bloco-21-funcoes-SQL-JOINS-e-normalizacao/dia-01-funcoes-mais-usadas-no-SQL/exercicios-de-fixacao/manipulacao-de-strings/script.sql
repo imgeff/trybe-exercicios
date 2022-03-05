e6a9f0695aebf658aed305042b2186297ff3b39c
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-05 18:48:39 -0300

    feat(condicionais): Query 1

--- Back-End/bloco-21-funcoes-SQL-JOINS-e-normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/manipulacao-de-strings/script.sql
+++
@@ -1,5 +0,0 @@
-SELECT UCASE('trybe');
-SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'google');
-SELECT CHAR_LENGTH('Uma frase qualquer');
-SELECT SUBSTR('A linguagem JavaScript está entre as mais usadas', 12, 11);
-SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
