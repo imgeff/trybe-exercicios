1140ee1f486cdfedba04cf2f87324ab817c26fd1
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-05 19:05:40 -0300

    feat(func-math): Query 4

--- Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/funcoes-matematicas/script.sql
+++ Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/funcoes-matematicas/script.sql
@@ -4,3 +4,5 @@ SELECT ROUND(15 + (RAND() * 5)) AS value;
 SELECT ROUND(15.7515971, 5) AS round_value;
 -- QUERY 3
 SELECT FLOOR(39.494) AS floor_value;
+-- QUERY 4
+SELECT CEIL(85.234) AS ceil_value;
