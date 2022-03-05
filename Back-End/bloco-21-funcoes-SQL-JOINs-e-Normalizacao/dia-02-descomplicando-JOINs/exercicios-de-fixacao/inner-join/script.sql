1303055b40fc575875e4c306ba00d8ce6846c743
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-05 18:08:58 -0300

    feat(innerJoin): Query 5

--- Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-02-descomplicando-JOINs/exercicios-de-fixacao/inner-join/script.sql
+++ Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-02-descomplicando-JOINs/exercicios-de-fixacao/inner-join/script.sql
@@ -23,3 +23,13 @@ FROM sakila.customer C
 INNER JOIN sakila.address A
 ON C.address_id = A.address_id
 HAVING A.district = 'California' AND LCASE(`name`) LIKE('%rene%');
+-- QUERY 5
+SELECT 
+CONCAT(c.first_name, ' ', c.last_name) AS `name`,
+COUNT(a.address_id) AS Total_Address
+FROM sakila.customer c
+INNER JOIN sakila.address a
+ON c.address_id = a.address_id
+WHERE c.active = 1
+GROUP BY `name`
+ORDER BY `name` DESC;
