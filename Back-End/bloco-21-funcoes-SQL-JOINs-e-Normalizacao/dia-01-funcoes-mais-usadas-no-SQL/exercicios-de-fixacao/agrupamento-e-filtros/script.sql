d9a0f171bab3eafa80792885f9637e428f60eb9e
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-05 21:31:34 -0300

    feat(agrupamento): Query 3

--- Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/agrupamento-e-filtros/script.sql
+++ Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/agrupamento-e-filtros/script.sql
@@ -11,3 +11,10 @@ active,
 COUNT(active)
 FROM sakila.customer
 GROUP BY store_id, active;
+-- QUERY 3
+SELECT
+rating,
+AVG(rental_duration) AS 'média de duração de locação'
+FROM sakila.film
+GROUP BY rating
+ORDER BY AVG(rental_duration) DESC;
