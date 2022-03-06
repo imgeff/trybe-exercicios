3fddae56e6aed84dc73c5842a1f7f93e64947932
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-05 21:23:13 -0300

    feat(agrupamento): Query 2

--- Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/agrupamento-e-filtros/script.sql
+++ Back-End/bloco-21-funcoes-SQL-JOINs-e-Normalizacao/dia-01-funcoes-mais-usadas-no-SQL/exercicios-de-fixacao/agrupamento-e-filtros/script.sql
@@ -4,3 +4,10 @@ active,
 COUNT(active) AS quantity_clients
 FROM sakila.customer
 GROUP BY active;
+-- QUERY 2
+SELECT 
+store_id,
+active,
+COUNT(active)
+FROM sakila.customer
+GROUP BY store_id, active;
