710c556e2172fb65675a9624f0792cebbc5c9414
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-15 21:21:02 -0300

    feat(ex-1): testa se a função retorna 'positivo'

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/index.js
@@ -1,5 +1,5 @@
-const sortingNumber = (number) => {
-
+const sortingNumber = (value) => {
+  if (value > 0) return 'positivo';
 }
 
 module.exports = sortingNumber;
--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/tests/app.test.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/tests/app.test.js
@@ -5,4 +5,8 @@ describe('Testa o funcionamento da função sortingNumber', () => {
   it('Verifica se existe a função sortingNumber', () => {
     expect(sortingNumber).to.be.a('function');
   })
+  it('retorna a string "positivo" para numero maior que 0', () => {
+    const result = sortingNumber(7);
+    expect(result).to.be.equal('positivo');
+  })
 })
