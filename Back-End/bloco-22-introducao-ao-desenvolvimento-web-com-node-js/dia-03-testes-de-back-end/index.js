f2ed4fb17ca00bc664a4a0965160a0a542bafc32
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-15 22:23:05 -0300

    feat(ex-4): testa e implementa função que escreve em arquivos

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const sortingNumber = (value) => {
   if (typeof value !== 'number') return 'o valor deve ser um número';
   if (value > 0) return 'positivo';
@@ -5,4 +7,15 @@ const sortingNumber = (value) => {
   if (value < 0) return 'negativo'
 }
 
-module.exports = sortingNumber;
+const writeInFile = (nameFile, textFile) => {
+  let message;
+  fs.writeFileSync(nameFile, textFile, 'utf-8', (err) => {
+    if(err) throw err;
+  });
+  return 'ok';
+}
+
+module.exports = {
+  sortingNumber,
+  writeInFile,
+};
--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/tests/app.test.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end/tests/app.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const sortingNumber = require('../index');
+const { sortingNumber, writeInFile } = require('../index');
 
 describe('Testa o funcionamento da função sortingNumber', () => {
   it('Verifica se existe a função sortingNumber', () => {
@@ -20,5 +20,16 @@ describe('Testa o funcionamento da função sortingNumber', () => {
   it('retorna a string "o valor deve ser um número" quando for passado um valor que não seja do type number', () => {
     const result = sortingNumber('7');
     expect(result).to.be.equal('o valor deve ser um número');
+  })
+})
+
+describe('Testa função que escreve em arquivos', () => {
+  it('Verifica se função existe', () => {
+    expect(writeInFile).to.be.a('function');
+  })
+  it('Verifica se a função retorna a string "ok"', () => {
+    const result = writeInFile('file.txt', 'texto do arquivo');
+    expect(result).to.be.a('string');
+    expect(result).to.be.equal('ok');
   })
 })
