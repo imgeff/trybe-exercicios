2471af6e94643a61684e4b189af20a6d58076c1b
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-16 22:21:26 -0300

    feat(ex-2): implementa metodo post na uri /hello

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-Express-http-com-node-js/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-Express-http-com-node-js/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', (_req, res) => {
   return res.status(200).json({ message: 'pong' }
   );
 })
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.status(201).json({ message: `Hello ${name}!` })
+})
+
 
 app.listen(3000, () => console.log('aplicação rodando na porta 3000'))
