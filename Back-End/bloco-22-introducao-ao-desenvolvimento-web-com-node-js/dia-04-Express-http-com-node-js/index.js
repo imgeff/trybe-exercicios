fe5b3b39522735e7d9983501f64fac8641a827a7
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-16 22:36:04 -0300

    feat(ex-3): implementa metodo post na uri /greeting

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-Express-http-com-node-js/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-Express-http-com-node-js/index.js
@@ -5,14 +5,20 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/ping', (_req, res) => {
-  return res.status(200).json({ message: 'pong' }
-  );
-})
+  return res.status(200).json({ message: 'pong' });
+});
 
 app.post('/hello', (req, res) => {
   const { name } = req.body;
-  return res.status(201).json({ message: `Hello ${name}!` })
-})
+  return res.status(201).json({ message: `Hello ${name}!` });
+});
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  const failApi = res.status(404).json({ message: 'Unauthorized' });
+  const sucessApi = res.status(200).json({ message: `Hello, ${name}!`});
+  if (parseInt(age) > 17) return sucessApi;
+  return failApi;
+});
 
-app.listen(3000, () => console.log('aplicação rodando na porta 3000'))
+app.listen(3000, () => console.log('aplicação rodando na porta 3000'));
