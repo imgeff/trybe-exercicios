1964c8cbb70865f50815b2bbc1c29bbdb3954d84
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-06 15:31:09 -0300

    feat: Altera rota login colocando condição admin

--- Back-End/bloco-24-ORM-e-autenticacao/dia-03-JWT/hello-jwt/controllers/login.js
+++ Back-End/bloco-24-ORM-e-autenticacao/dia-03-JWT/hello-jwt/controllers/login.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 const jwt = require('jsonwebtoken');
 
 // eslint-disable-next-line no-unused-expressions
@@ -12,10 +13,14 @@ const secret = process.env.SECRET;
 
 const login = (req, res) => {
   const { username, password } = req.body;
+  const payload = { username, admin: false };
   if (!username || username.length < 5 || !password || password.length < 5) {
     return res.status(401).json({ message: 'username e password são obrigatórios' });
   }
-  const token = jwt.sign({ username, admin: false }, secret, jwtConfig);
+  if (username === 'admin' && password === 's3nh4S3gur4???') {
+    payload.admin = true;
+  }
+  const token = jwt.sign(payload, secret, jwtConfig);
   return res.status(200).json({ token });
 };
 
