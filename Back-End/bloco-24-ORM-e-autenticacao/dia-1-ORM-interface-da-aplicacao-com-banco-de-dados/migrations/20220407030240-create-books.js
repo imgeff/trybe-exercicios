5ac299c0cfb0412a6a21cdd025cff35cc6b6b939
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-07 14:34:23 -0300

    feta: cria migration books

---
+++ Back-End/bloco-24-ORM-e-autenticacao/dia-1-ORM-interface-da-aplicacao-com-banco-de-dados/migrations/20220407030240-create-books.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    }, {
+      timeStamp: false,
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
