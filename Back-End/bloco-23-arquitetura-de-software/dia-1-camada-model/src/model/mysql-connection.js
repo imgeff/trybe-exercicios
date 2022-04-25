74b90638abb848f5f0d26e0f46a22f5ae51a8a32
Gefferson Batista <82794410+imgeff@users.noreply.github.com>
2022-04-25 11:55:38 -0300

    Delete mysql-connection.js

--- Back-End/bloco-23-arquitetura-de-software/dia-1-camada-model/src/model/mysql-connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-  user: "imgeff",
-  host: "localhost",
-  password: "Imgeff1822*",
-});
-
-module.exports = connection;
