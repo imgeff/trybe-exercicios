03c3605359849f7ca5fddc81f7dd5f062066a42e
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-27 18:28:33 -0300

    feat: sobe banco de dados com supabase

---
+++ Back-End/bloco-25-deployment/dia-2-deploy-docker-e-heroku/supabase-with-sequelize/index.js
@@ -0,0 +1,23 @@
+// arquivo/index.js
+
+const express = require('express')
+
+const { Product } = require('./models')
+
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.post('/product', async (req, res) => {
+
+  const { name, description } = req.body;
+
+  const product = await Product.create({ name, description });
+
+  return res.status(201).json(product);
+});
+
+app.listen(port, () => console.log(`Servidor online na porta ${port}`));
