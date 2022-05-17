a866119e69bc0aad917a267cb24845f05712cd5c
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-17 20:53:38 -0300

    feat: Exercicio 4
    cria os metodos totalPedido e descontoPedido na class Pedido

--- Back-End/bloco-27-POO-e-SOLID/dia-1-introducao/src/models/Entities/Pedido.ts
+++ Back-End/bloco-27-POO-e-SOLID/dia-1-introducao/src/models/Entities/Pedido.ts
@@ -13,6 +13,19 @@ class Pedido {
     this.formaDePagamento = formaDePagamento;
     this.percentual = percentual;
   }
+
+  totalPedido () {
+    const pedidoItems = this.items;
+    let total: number = 0;
+    pedidoItems.forEach((item) => total += item.preco )
+    return total;
+  }
+
+  descontoPedido () {
+    const total = this.totalPedido();
+    const totalComDesconto = total - (total * this.percentual);
+    return totalComDesconto;
+  }
 }
 
 export default Pedido;
