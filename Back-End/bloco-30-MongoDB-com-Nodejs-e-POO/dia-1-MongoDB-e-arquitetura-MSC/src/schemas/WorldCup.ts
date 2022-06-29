e16c18619700971ca2bbee349b559b5fcdacb8df
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-06-29 19:56:31 -0300

    :bug: fix: tipagem de WorldCupSchema

--- Back-End/bloco-30-MongoDB-com-Nodejs-e-POO/dia-1-MongoDB-e-arquitetura-MSC/src/schemas/WorldCup.ts
+++ Back-End/bloco-30-MongoDB-com-Nodejs-e-POO/dia-1-MongoDB-e-arquitetura-MSC/src/schemas/WorldCup.ts
@@ -18,7 +18,7 @@ export const WorldCupSchema = new Schema<IWorldCup>({
   champions: { type: String, required: true },
   runnerUp: { type: String, required: true },
   editionGoals: { type: Number, required: true },
-  editionStrikers: { type: [String], required: true },
+  editionStrikers: { type: [{ type: String }], required: true },
   bestPlayer: { type: String, required: true },
   bestGoalkeeper: { type: String, required: true },
   bestYoungPlayer: { type: String, required: true },
