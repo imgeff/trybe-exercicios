a9d8e628cd4320b5c0c3cac35107e82b8a7a8280
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-26 20:01:29 -0300

    :recycle: refactor: Refatora função count_even
    utiliza recursão para calcular a quantidade de números pares

--- Ciencia-da-Computacao/bloco-36/dia-2-recursividade-e-estratégias-para-solução-de-problemas/operation_numbers.py
+++ Ciencia-da-Computacao/bloco-36/dia-2-recursividade-e-estratégias-para-solução-de-problemas/operation_numbers.py
@@ -1,11 +1,25 @@
 
 def count_even(numbers):
     quantity_even = 0
-    for number in numbers:
-        if number % 2 == 0:
+    if len(numbers) <= 1:
+        if numbers[0] % 2 == 0:
             quantity_even += 1
-    return quantity_even
+        return quantity_even
+    if numbers[-1] % 2 == 0:
+        quantity_even += 1
+    numbers = numbers[:-1]
+    return quantity_even + count_even(numbers)
 
+
+# 0 4
+# 1 3
+# 0 3
+# 1 2
+# 0 2
+# 1 1
+# 0 1
+# 1 0
+# 0
 
 if __name__ == "__main__":
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
